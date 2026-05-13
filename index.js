11c8c36838fddb13d99b0946c1477b37e1d8b297
Paul Bromwell <pbromwelljr@gmail.com>
2026-05-13 15:50:34 -0400

    Debugging.

--- index.js
+++ index.js
@@ -11,98 +11,143 @@ let trackers = [];
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                              Main
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-Hooks.once('init', async () => {
-	await registerSettings();
-	console.log(`${moduleTag} | Initialized`);
+Hooks.once('init', () => {
+    console.log(`${moduleTag} | Initializing...`);
+    registerSettings().then(() => {
+        console.log(`${moduleTag} | Initialized`);
+    }).catch((e) => {
+        console.error(`${moduleTag} | Error during initialization: ${e}`);
+    });
 });
 
 Hooks.on('socketlib.ready', () => {
-	socket = socketlib.registerModule(moduleName);
-	socket.register('recoverClient', recoverClient);
+    console.log(`${moduleTag} | Socketlib ready. Registering module ${moduleName}...`);
+    socket = socketlib.registerModule(moduleName);
+    console.log(`${moduleTag} | Registering recover client...`);
+    socket.register('recoverClient', recoverClient);
 });
 
 Hooks.on('ready', async () => {
-	Hooks.callAll('ammo-tracker.ready', AmmoTracker);
+    console.log(`${moduleTag} | ready event handler start`);
+    Hooks.callAll('ammo-tracker.ready', AmmoTracker);
 
-	// Enable watcher.
-	watcher();
-	if (!game.user.isGM) return;
+    // Enable watcher.
+    console.log(`${moduleTag} | Enabling watcher...`);
+    watcher();
 
-	// Fetch running combats and create trackers
-	const combats = game.combats._source;
+    if (!game.user.isGM) {
+        console.log(`${moduleTag} | Non-GM detected. Returning.`);
+        return;
+    }
 
-	for (let combat of combats) {
-		let tracker = new AmmoTracker(combat._id, true);
-		if (tracker.combat.round !== 0) tracker.started = true;
-		trackers.push(tracker);
-	}
+    // Fetch running combats and create trackers
+    const combats = game.combats._source;
 
-	console.log(`${moduleTag} | Ready`);
+    for (let combat of combats) {
+        console.log(`${moduleTag} | Adding ammo tracker for existing combat ${combat._id}`);
+        let tracker = new AmmoTracker(combat._id, true);
+        if (tracker.combat.round !== 0) {
+            console.log(`${moduleTag} | Combat ${combat._id} has already started, starting tracker...`);
+            tracker.started = true;
+        }
+        trackers.push(tracker);
+    }
+
+    console.log(`${moduleTag} | Ready`);
 });
 
 Hooks.on('createCombat', async (...args) => {
-	if (!game.user.isGM) return;
-	const system = game.system.id;
-	const tracker = new systemTrackers[system](args[0]._id);
-	console.log(`${moduleTag} | Created new tracker.`);
-	trackers.push(tracker);
+    console.log(`${moduleTag} | Combat creation detected...`);
+    if (!game.user.isGM) {
+        console.log(`${moduleTag} | Non-GM user detected. Returning.`);
+        return;
+    }
+    const system = game.system.id;
+    console.log(`${moduleTag} | Using game system ${game.system.id} for combat ${args[0]._id}...`);
+
+    const tracker = new systemTrackers[system](args[0]._id);
+    console.log(`${moduleTag} | Created new tracker.`);
+    trackers.push(tracker);
 });
 
 Hooks.on('updateCombat', async (...args) => {
-	if (!game.user.isGM) return;
-	if (args[0].round === 0) {
-		return true;
-	}
+    console.log(`${moduleTag} | Combat update detected...`);
+    if (!game.user.isGM) {
+        console.log(`${moduleTag} | Non-GM user detected. Returning.`);
+        return;
+    }
+    if (args[0].round === 0) {
+        console.log(`${moduleTag} | Combat has not started yet. Returning.`);
+        return true;
+    }
 
-	for (let tracker of trackers) {
-		if (tracker.combatId == args[0]._id) {
-			if (!tracker.started) {
-				tracker.started = true;
-				await tracker.startTracker();
-			}
-			break;
-		}
-	}
+    let trackerStarted = false;
+    for (let tracker of trackers) {
+        if (tracker.combatId == args[0]._id) {
+            if (!tracker.started) {
+                tracker.started = true;
+                await tracker.startTracker();
+            }
+            trackerStarted = true;
+            break;
+        }
+    }
+
+    if (trackerStarted) {
+        console.log(`${moduleTag} | Combat tracker was successfully started for combat ${args[0]._id}`);
+    } else {
+        console.log(`${moduleTag} | No combat tracker was found for combat ${args[0]._id}`);
+    }
 });
 
 Hooks.on('deleteCombat', async (...args) => {
-	if (!game.user.isGM) return;
-	for (let tracker of trackers) {
-		if (tracker.combatId == args[0]._id) {
-			if (tracker.started) {
-				tracker.ended = true;
-				await tracker.endTracker();
-				console.log(`${moduleTag} | Ended tracking.`);
-			}
-		}
-	}
+    if (!game.user.isGM) {
+        console.log(`${moduleTag} | Non-GM user detected. Returning.`);
+        return;
+    }
+
+    let trackerEnded = false;
+    for (let tracker of trackers) {
+        if (tracker.combatId == args[0]._id) {
+            if (tracker.started) {
+                tracker.ended = true;
+                await tracker.endTracker();
+                console.log(`${moduleTag} | Ended tracking.`);
+                trackerEnded = true;
+            }
+        }
+    }
+
+    if (!trackerEnded) {
+        console.log(`${moduleTag} | Failed to end tracker for combat ${args[0]._id}`);
+    }
 });
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                            Watcher
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-function watcher() {
-	$(document).on('click', '.at-recover-btn', async button => {
-		const dataset = button.currentTarget.dataset;
-		if (!game.user.isGM) {
-			socket.executeAsGM(recoverClient, dataset);
-			return;
-		}
+function watcher () {
+    $(document).on('click', '.at-recover-btn', async button => {
+        const dataset = button.currentTarget.dataset;
+        if (!game.user.isGM) {
+            socket.executeAsGM(recoverClient, dataset);
+            return;
+        }
 
-		await recoverClient(dataset);
-	});
+        await recoverClient(dataset);
+    });
 }
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                       Recover - Client
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 export const recoverClient = async function (dataset) {
-	let currentTracker = trackers.find(
-		tracker => tracker.combatId == dataset.combatId
-	);
-	console.debug(currentTracker);
+    let currentTracker = trackers.find(
+        tracker => tracker.combatId == dataset.combatId
+    );
+    console.debug(currentTracker);
 
-	if (currentTracker != undefined) {
-		await currentTracker.recover(dataset.actorId);
-	}
+    if (currentTracker != undefined) {
+        await currentTracker.recover(dataset.actorId);
+    }
 };
--- scripts/settings.js
+++ scripts/settings.js
@@ -6,21 +6,23 @@ import { moduleName, moduleTag } from './constants.js';
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                            Imports
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-export async function registerSettings() {
-	await game.settings.register(moduleName, 'trackMagic', {
-		name: '[Experimental] Track Magical Ammunition',
-		scope: 'world',
-		config: true,
-		type: Boolean,
-		default: false,
-	});
-
-	await game.settings.register(moduleName, 'chat-trackers', {
-		name: 'Internal Trackers',
-		scope: 'world',
-		config: false,
-		type: Object,
-	});
+export function registerSettings () {
+    console.log(`${moduleTag} | Registering trackMagic for module ${moduleName}...`);
+    return game.settings.register(moduleName, 'trackMagic', {
+        name: '[Experimental] Track Magical Ammunition',
+        scope: 'world',
+        config: true,
+        type: Boolean,
+        default: false,
+    }).then(() => {
+        console.log(`${moduleTag} | Registering chat-trackers for module ${moduleName}...`);
+        return game.settings.register(moduleName, 'chat-trackers', {
+            name: 'Internal Trackers',
+            scope: 'world',
+            config: false,
+            type: Object,
+        });
+    });
 }
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                            Imports
